b3c6412a3a124a22e039cc53427e8e63754a094a
truongtuan <tuanoker@gmail.com>
2022-01-10 15:44:11 +0700

    test

--- lesson1/bt1.php
+++ lesson1/bt1.php
@@ -1,11 +1,5 @@
 <?php
-// tạo lớp ô tô có các thuộc tính tên model, màu xe, 
-// năm sản xuất, hãng, biển số xe
-// có các phương thức: bán, chuyển nhượng, đăng ký biển số
-//  xe
-// tạo hàm khởi tạo để gán giá trị cho các thuộc tính 
-// của class ô tô
-
-
+$a = 123;
+echo $a;
 
 ?>
